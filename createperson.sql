d2acd8fcaabfe15d215358dd31f0573c156570a9
Denis Gitelman <den.gitelman@gmail.com>
2024-03-23 23:04:06 +0300

    Initial commit

---
+++ createperson.sql
@@ -0,0 +1,8 @@
+CREATE TABLE PERSONS(
+    name varchar(50),
+    surname varchar(50),
+    age int,
+    phone_number varchar(20),
+    city_of_living varchar(30),
+    PRIMARY KEY (name,surname,age)
+);
